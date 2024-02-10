4c82d5aa0a916fa887475db8c2c1e8b1e33beacf
iFanatical <theawpportunist@gmail.com>
2024-02-09 23:32:17 -0700

    Updated install script again!

--- program-list.ps1
+++ program-list.ps1
@@ -1,38 +1,267 @@
+Write-Host "    _    ____                                "
+Write-Host "   / \  | __ )	https://github.com/iFanatical"
+Write-Host "  / _ \ |  _ \                               "
+Write-Host " / ___ \| |_) | Windows Automation Script    "
+Write-Host "/_/   \_\____/  	                         "
+#
+#
+#
 # Install general programs
-winget install Bitwarden.Bitwarden
-winget install Upscayl.Upscayl
-winget install ShareX.ShareX
-winget install Audacity.Audacity
-winget install AutoHotkey.AutoHotkey
-winget install CPUID.CPU-Z
-winget install CPUID.HWMonitor
-winget install Discord.Discord
-winget install GIMP.GIMP
-winget install GitHub.GitHubDesktop
-winget install Git.Git
-winget install Gyan.FFmpeg
+Write-Host "--------------------------------------"
+Write-Host "Installing browsers"
+Write-Host "--------------------------------------"
+Write-Host "Installing Firefox"
+Write-Host "--------------------------------------"
 winget install Mozilla.Firefox
-winget install Notepad++.Notepad++
-winget install Insecure.Npcap
-winget install OBSProject.OBSStudio
-winget install PrismLauncher.PrismLauncher
-winget install Rufus.Rufus
-winget install StartIsBack.StartAllBack
+Write-Host "--------------------------------------"
+Write-Host "Installing Thorium"
+Write-Host "--------------------------------------"
 winget install Alex313031.Thorium.AVX2
-winget install KDE.Kdenlive
-winget install qBittorrent.qBittorrent
-winget install WinSCP.WinSCP
-winget install Microsoft.PowerToys
-winget install 7zip.7zip
-winget install AdrienAllard.FileConverter
-winget install RedHat.VirtViewer
-winget install Neovim.Neovim
+#
+#
+#
+Write-Host "--------------------------------------"
+Write-Host "Installing image based utilities"
+Write-Host "--------------------------------------"
+Write-Host "Installing Upscayl"
+Write-Host "--------------------------------------"
+winget install Upscayl.Upscayl
+Write-Host "--------------------------------------"
+Write-Host "Installing ShareX"
+Write-Host "--------------------------------------"
+winget install ShareX.ShareX
+Write-Host "--------------------------------------"
+Write-Host "Installing ImageGlass"
+Write-Host "--------------------------------------"
 winget install DuongDieuPhap.ImageGlass
-winget install Starship.Starship
-winget install Python.Launcher
+Write-Host "--------------------------------------"
+Write-Host "Installing GIMP"
+Write-Host "--------------------------------------"
+winget install GIMP.GIMP
+#
+#
+#
+Write-Host "--------------------------------------"
+Write-Host "Installing audio and video utilities"
+Write-Host "--------------------------------------"
+Write-Host "Installing Audacity"
+Write-Host "--------------------------------------"
+winget install Audacity.Audacity
+Write-Host "--------------------------------------"
+Write-Host "Installing Kdenlive"
+Write-Host "--------------------------------------"
+winget install KDE.Kdenlive
+Write-Host "--------------------------------------"
+Write-Host "Installing FFmpeg"
+Write-Host "--------------------------------------"
+winget install Gyan.FFmpeg
+Write-Host "--------------------------------------"
+Write-Host "Installing OBSStudio"
+Write-Host "--------------------------------------"
+winget install OBSProject.OBSStudio
+Write-Host "--------------------------------------"
+Write-Host "Installing VLC"
+Write-Host "--------------------------------------"
 winget install VideoLAN.VLC
-winget install PuTTY.PuTTY
-winget install WiresharkFoundation.Wireshark
+#
+#
+#
+Write-Host "--------------------------------------"
+Write-Host "Installing local utilities"
+Write-Host "--------------------------------------"
+Write-Host "Installing AutoHotkey"
+Write-Host "--------------------------------------"
+winget install AutoHotkey.AutoHotkey
+Write-Host "--------------------------------------"
+Write-Host "Installing CPU-Z"
+Write-Host "--------------------------------------"
+winget install CPUID.CPU-Z
+Write-Host "--------------------------------------"
+Write-Host "Installing HWMonitor"
+Write-Host "--------------------------------------"
+winget install CPUID.HWMonitor
+Write-Host "--------------------------------------"
+Write-Host "Installing Rufus"
+Write-Host "--------------------------------------"
+winget install Rufus.Rufus
+Write-Host "--------------------------------------"
+Write-Host "Installing VirtViewer"
+Write-Host "--------------------------------------"
+winget install RedHat.VirtViewer
+Write-Host "--------------------------------------"
+Write-Host "Installing 7zip"
+Write-Host "--------------------------------------"
+winget install 7zip.7zip
+Write-Host "--------------------------------------"
+Write-Host "Installing PowerShell"
+Write-Host "--------------------------------------"
 winget install Microsoft.PowerShell
-winget install Famatech.AdvancedIPScanner
+Write-Host "--------------------------------------"
+Write-Host "Installing Python Launcher"
+Write-Host "--------------------------------------"
+winget install Python.Launcher
+Write-Host "--------------------------------------"
+Write-Host "Installing Python 3.11"
+Write-Host "--------------------------------------"
 winget install Python.Python.3.11
+#
+#
+#
+Write-Host "--------------------------------------"
+Write-Host "Installing network utilities"
+Write-Host "--------------------------------------"
+Write-Host "Installing Npcap"
+Write-Host "--------------------------------------"
+winget install Insecure.Npcap
+Write-Host "--------------------------------------"
+Write-Host "Installing Wireshark"
+Write-Host "--------------------------------------"
+winget install WiresharkFoundation.Wireshark
+Write-Host "--------------------------------------"
+Write-Host "Installing WinSCP"
+Write-Host "--------------------------------------"
+winget install WinSCP.WinSCP
+Write-Host "--------------------------------------"
+Write-Host "Installing PuTTY"
+Write-Host "--------------------------------------"
+winget install PuTTY.PuTTY
+Write-Host "--------------------------------------"
+Write-Host "Installing AdvancedIPScanner"
+Write-Host "--------------------------------------"
+winget install Famatech.AdvancedIPScanner
+#
+#
+#
+Write-Host "--------------------------------------"
+Write-Host "Installing internet related applications"
+Write-Host "--------------------------------------"
+Write-Host "Installing qBittorrent"
+Write-Host "--------------------------------------"
+winget install qBittorrent.qBittorrent
+Write-Host "--------------------------------------"
+Write-Host "Installing Bitwarden"
+Write-Host "--------------------------------------"
+winget install Bitwarden.Bitwarden
+#
+#
+#
+Write-Host "--------------------------------------"
+Write-Host "Installing text editors"
+Write-Host "--------------------------------------"
+Write-Host "Installing Notepad++"
+Write-Host "--------------------------------------"
+winget install Notepad++.Notepad++
+Write-Host "--------------------------------------"
+Write-Host "Installing VisualStudioCode"
+Write-Host "--------------------------------------"
+winget install Microsoft.VisualStudioCode
+Write-Host "--------------------------------------"
+Write-Host "Installing Neovim"
+Write-Host "--------------------------------------"
+winget install Neovim.Neovim
+#
+#
+#
+Write-Host "--------------------------------------"
+Write-Host "Installing gaming utilities"
+Write-Host "--------------------------------------"
+Write-Host "Installing Discord"
+Write-Host "--------------------------------------"
+winget install Discord.Discord
+Write-Host "--------------------------------------"
+Write-Host "Installing PrismLauncher"
+Write-Host "--------------------------------------"
+winget install PrismLauncher.PrismLauncher
+#
+#
+#
+Write-Host "--------------------------------------"
+Write-Host "Installing Git utilities"
+Write-Host "--------------------------------------"
+Write-Host "Installing GitHubDesktop"
+Write-Host "--------------------------------------"
+winget install GitHub.GitHubDesktop
+Write-Host "--------------------------------------"
+Write-Host "Installing Git"
+Write-Host "--------------------------------------"
+winget install Git.Git
+#
+#
+#
+Write-Host "--------------------------------------"
+Write-Host "Installing Windows improvements"
+Write-Host "--------------------------------------"
+Write-Host "Installing PowerToys"
+Write-Host "--------------------------------------"
+winget install Microsoft.PowerToys
+Write-Host "--------------------------------------"
+Write-Host "Installing StartAllBack"
+Write-Host "--------------------------------------"
+winget install StartIsBack.StartAllBack
+Write-Host "--------------------------------------"
+Write-Host "Installing Starship"
+Write-Host "--------------------------------------"
+winget install Starship.Starship
+Write-Host "--------------------------------------"
+Write-Host "Installing FileConverter"
+Write-Host "--------------------------------------"
+winget install AdrienAllard.FileConverter
+#
+#
+#
+# Copy files to the documents folder
+Write-Host "--------------------------------------"
+Write-Host "Copying 'Documents' folder - AHK, PowerShell, etc"
+Write-Host "--------------------------------------"
+robocopy H:\Backup\Documents $Env:UserProfile\Documents /e /w:0 /r:0
+Write-Host "--------------------------------------"
+Write-Host "Copying 'Music' folder"
+Write-Host "--------------------------------------"
+robocopy H:\Backup\Music $Env:UserProfile\Music /e /w:0 /r:0
+Write-Host "--------------------------------------"
+Write-Host "Copying 'projects' folder"
+Write-Host "--------------------------------------"
+robocopy H:\Backup\projects $Env:UserProfile\projects /e /w:0 /r:0
+Write-Host "--------------------------------------"
+Write-Host "Copying Windows Terminal settings.json file"
+Write-Host "--------------------------------------"
+robocopy $Env:UserProfile\projects\windows-files\settings.json $Env:UserProfile\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json /e /w:0 /r:0
+Write-Host "--------------------------------------"
+Write-Host "Copying videos"
+Write-Host "--------------------------------------"
+robocopy H:\Backup\Videos $Env:UserProfile\Videos /e /w:0 /r:0
+Write-Host "--------------------------------------"
+Write-Host "Copying .config files"
+Write-Host "--------------------------------------"
+robocopy H:\Backup\.config $Env:UserProfile\.config /e /w:0 /r:0
+Write-Host "--------------------------------------"
+Write-Host "Copying .ssh keys"
+Write-Host "--------------------------------------"
+robocopy H:\Backup\.ssh $Env:UserProfile\.ssh /e /w:0 /r:0
+Write-Host "--------------------------------------"
+Write-Host "Copying hosts file"
+Write-Host "--------------------------------------"
+robocopy H:\Backup\hosts C:\Windows\System32\drivers\etc\hosts /e /w:0 /r:0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
