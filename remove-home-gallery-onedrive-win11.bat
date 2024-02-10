88d8cdef167b29ddf49c4462f898990924a91dcb
Aaron Bush <theawpportunist@gmail.com>
2024-02-10 13:03:01 -0700

    Script to remove pinned Home/Gallery/OneDrive

---
+++ remove-home-gallery-onedrive-win11.bat
@@ -0,0 +1,3 @@
+reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace_41040327\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace_36354489\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}
+reg add HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6} /v System.IsPinnedToNameSpaceTree /t REG_DWORD /d 0 /f
