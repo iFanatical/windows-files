0c9b8aa95a874429272c49c50e88557d71e0ff67
iFanatical <theawpportunist@gmail.com>
2024-02-09 21:26:04 -0700

    Updated MS PowerShell with extra functions/aliases

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,3 @@
-
 #34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
 
 Import-Module "C:\Users\fanatical\AppData\Local\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
@@ -7,6 +6,10 @@ Import-Module "C:\Users\fanatical\AppData\Local\PowerToys\WinUI3Apps\..\WinGetCo
 #Aliases
 function mcserver { ssh 192.168.1.16 }
 function sftp-server { ssh 10.1.30.11 }
+function proxmox01 { ssh root@10.1.10.5 }
+function proxmox02 { ssh root@10.1.10.6 }
+function poweroff { Stop-Computer -Force }
+function reboot { Restart-Computer -Force }
 
 #Starship
 Invoke-Expression (&starship init powershell)
