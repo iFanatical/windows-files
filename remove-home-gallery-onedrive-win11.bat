a3141037c4a41e958a5865a0d813cf0d9b63f855
Aaron Bush <theawpportunist@gmail.com>
2024-02-10 13:11:06 -0700

    Updated script to include comments and disable Defender Notifications

--- remove-home-gallery-onedrive-win11.bat
+++ remove-home-gallery-onedrive-win11.bat
@@ -1,3 +1,9 @@
+:: Remove Home and Gallery
 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace_41040327\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}
 reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace_36354489\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}
+
+:: Remove OneDrive
 reg add HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6} /v System.IsPinnedToNameSpaceTree /t REG_DWORD /d 0 /f
+
+:: Disable Windows Defender Notifications
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\Notifications" /v DisableNotifications /t REG_DWORD /d 1 /f
